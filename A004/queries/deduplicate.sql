81f34e625c9ed9ea486c536e86e72e9e19fe76e2
Clara Verena Battesini <clarabattesini@gmail.com>
2021-11-09 18:50:39 -0300

    Last lesson inserting data into a view

--- A004/queries/deduplicate.sql
+++ A004/queries/deduplicate.sql
@@ -14,7 +14,7 @@ CREATE TABLE tb_web_site AS (
     WHERE t1.dedup = 1
 );
 
-SELECT * FROM tb_web_site
+SELECT * FROM tb_web_site;
 
 CREATE TABLE tb_artist AS (
     SELECT t1."date"
@@ -24,9 +24,9 @@ CREATE TABLE tb_artist AS (
         FROM PUBLIC."Billboard" as t1
         WHERE t1.artist = 'AC/DC'
         ORDER BY t1.artist, t1."date"
-)
+);
 
-SELECT * FROM tb_artist
+SELECT * FROM tb_artist;
 
 CREATE VIEW view_artist AS (
     WITH cte_dedup_table AS (
@@ -44,4 +44,34 @@ CREATE VIEW view_artist AS (
     WHERE t1.dedup = 1
 );
 
-SELECT * FROM view_artist
+-- DROP VIEW view_artist
+
+SELECT * FROM view_artist;
+
+INSERT INTO tb_artist (
+    SELECT t1.date
+        ,t1.rank
+        ,t1.artist
+        ,t1.song
+    FROM public."Billboard" AS t1
+    WHERE t1.artist LIKE 'Elvis%'
+    ORDER BY t1.artist, t1.song, t1.date
+);
+
+CREATE VIEW view_song AS (
+    WITH cte_dedup_table AS (
+        SELECT t1.date
+            ,t1.rank
+            ,t1.artist
+            ,t1.song
+            ,row_number() OVER(PARTITION BY artist ORDER by artist, song, date) as "dedup"
+        FROM tb_artist as t1
+        ORDER BY t1.artist, t1.song, t1.date
+    )
+    SELECT t1.date
+        ,t1.rank
+        ,t1.artist
+        ,t1.song
+    FROM cte_dedup_table AS t1
+    WHERE t1.dedup = 1
+);
