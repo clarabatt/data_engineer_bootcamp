0c5e34ebce082a8de73ae70af47467047ecdc10e
Clara Battesini <clarabattesini@gmail.com>
2022-03-01 15:25:32 -0500

    learned: how to get information of pages using selenium

--- A006/main.py
+++ A006/main.py
@@ -1,29 +1,50 @@
 #%%
+import sys
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-
-
 #%%
-driver = webdriver.Chrome('./src/chromedriver')
-driver.get('https://howedu.com.br')
 
-# %%
-driver.find_element(By.XPATH, '//*[@id="post-37"]/div/div/div/section[1]/div/div[1]/div/div[3]/div/div/a').click()
+cep = sys.argv[1]
 
-# %%
-driver.get('https://buscacepinter.correios.com.br/app/endereco/index.php')
-elem_cep = driver.find_element(By.NAME, 'endereco')
+if cep:
+    driver = webdriver.Chrome('./src/chromedriver')
+    time.sleep(1)
 
-# %%
-elem_cep.clear()
-elem_cep.send_keys('41940040')
+    #%%
+    # driver.get('https://howedu.com.br')
+    # driver.find_element(By.XPATH, '//*[@id="post-37"]/div/div/div/section[1]/div/div[1]/div/div[3]/div/div/a').click()
 
-# %%
-elem_type = Select(driver.find_element(By.NAME, 'tipoCEP'))
+    # %%
+    driver.get('https://buscacepinter.correios.com.br/app/endereco/index.php')
+    elem_cep = driver.find_element(By.NAME, 'endereco')
 
-# %%
-elem_type.select_by_value('LOG')
+    # %%
+    elem_cep.clear()
+    elem_cep.send_keys('41940040')
 
-# %%
-driver.find_element(By.ID, 'btn_pesquisar').click()
+    # %%
+    elem_type = Select(driver.find_element(By.NAME, 'tipoCEP'))
+    elem_type.select_by_value('LOG')
+    driver.find_element(By.ID, 'btn_pesquisar').click()
+    time.sleep(1)
+    # %%
+    street = driver.find_element(By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[1]').text
+    neighbourhood = driver.find_element(By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[2]').text
+    city = driver.find_element(By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[3]').text
+
+    driver.close()
+    # %%
+    print("""
+    Para o CEP {} temos:
+    Endereço: {}
+    Bairro: {}
+    Localidade: {}
+    """.format(
+    cep,
+    street.split(' - ')[0],
+    neighbourhood,
+    city
+    ))
