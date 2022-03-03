2c1c08c1422496b25f6dc5eea40115b900b61467
Clara Battesini <clarabattesini@gmail.com>
2022-03-03 14:51:26 -0500

    learned: check if the element exists, if not try again

--- A006/all_movies.py
+++ A006/all_movies.py
@@ -9,14 +9,35 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome('./src/chromedriver')
 time.sleep(5)
 driver.get('https://pt.wikipedia.org/wiki/Nicolas_Cage')
-table = driver.find_element(By.XPATH, '//*[@id="mw-content-text"]/div[1]/table[2]')
+table_xpath = '//*[@id="mw-content-text"]/div[1]/table[2]'
+
+
+#%%
+def has_item(xpath: str, drv=driver):
+    try:
+        drv.find_element(By.XPATH, xpath)
+        return True
+    finally:
+        return False
+
+
+i = 0
+while not has_item(table_xpath):
+    i += 1
+    if i > 50:
+        break
+    pass
+
+#%%
+table = driver.find_element(By.XPATH, table_xpath)
+
 driver.close()
 
-#%%
 df = pd.read_html('<table>' + table.get_attribute('innerHTML') + '</table>')[0]
 
+
 #%%
-df[df['Ano']==1984]
+print(df[df['Ano'] == 1984])
 
 #%%
 df.to_csv('nicolas_cage_movies.csv', sep=';', index=False)
