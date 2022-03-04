5d7fac2b887b82cb64e28553ac1e1a84bd4f5142
Clara Battesini <clarabattesini@gmail.com>
2022-03-03 22:11:21 -0500

    learn: how to parametrize a test

--- A005_2/tests/test_apis.py
+++ A005_2/tests/test_apis.py
@@ -1,20 +1,18 @@
 import datetime
-
+import pytest
 from apis import DaySummaryApi
 
 
-def test_get_endpoint_api_btc():
-    date = datetime.date(2021, 6, 21)
-    api = DaySummaryApi(coin="BTC")
+@pytest.mark.parametrize(
+    "coin, date, expected",
+    [
+        ("BTC", datetime.date(2021, 6, 21), "https://www.mercadobitcoin.net/api/BTC/day-summary/2021/6/21"),
+        ("ETH", datetime.date(2021, 6, 21), "https://www.mercadobitcoin.net/api/ETH/day-summary/2021/6/21"),
+        ("ETH", datetime.date(2019, 1, 2), "https://www.mercadobitcoin.net/api/ETH/day-summary/2019/1/2"),
+    ]
+)
+def test_get_endpoint(coin, date, expected):
+    api = DaySummaryApi(coin=coin)
     actual = api._get_end_point(date=date)
-    expected = "https://www.mercadobitcoin.net/api/BTC/day-summary/2021/6/21"
-    assert actual == expected
-
-
-def test_get_endpoint_api_eth():
-    date = datetime.date(2021, 6, 21)
-    api = DaySummaryApi(coin="ETH")
-    actual = api._get_end_point(date=date)
-    expected = "https://www.mercadobitcoin.net/api/BTC/day-summary/2021/6/21"
     assert actual == expected
 
